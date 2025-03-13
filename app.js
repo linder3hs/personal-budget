b302dc5351f1763704d3b12e7e4247bd2bee76ed
linder3hs <linderhassinger00@gmail.com>
2025-03-12 21:26:51 -0500

    feat: add ui to movement

--- app.js
+++ app.js
@@ -1,3 +1,50 @@
+// obtener la información del formulario
+// mediante el evento submit
+
+const form = document.querySelector("form");
+
+const transacciones = [];
+
+function getDataFromForm() {
+  const formData = new FormData(form);
+  const description = formData.get("description");
+  const amount = formData.get("amount");
+  const type = formData.get("type");
+
+  return {
+    description,
+    amount,
+    type,
+  };
+}
+
+function createMovement(movement) {
+  const nuevoMovimiento = new Movimiento(
+    movement.type,
+    movement.amount,
+    movement.description
+  );
+
+  const validacion = nuevoMovimiento.validarMovimiento();
+
+  if (validacion.ok) {
+    transacciones.push(nuevoMovimiento);
+    alert(validacion.message);
+    form.reset();
+  } else {
+    alert(validacion.message);
+  }
+}
+
+// escuchamos el evento submit
+form.addEventListener("submit", function (event) {
+  // ejecutamos los pasos para guardar la información
+  event.preventDefault();
+
+  const newMovement = getDataFromForm();
+  createMovement(newMovement);
+});
+
 // 1 -> Ingreso
 // 2 -> Egreso
 function Movimiento(tipo, monto, descripcion) {
@@ -5,6 +52,10 @@ function Movimiento(tipo, monto, descripcion) {
   this.monto = monto;
   this.descripcion = descripcion;
 }
+
+// Movimiento.prototype.render = function () {
+//   // insertar la informacion en HTML
+// };
 
 // Retorne un boolean (true | false)
 Movimiento.prototype.validarMovimiento = function () {
@@ -25,9 +76,8 @@ Movimiento.prototype.validarMovimiento = function () {
       message: "Debe completar la descripcion",
     };
   }
-
   // solo aceptamos 1 y 2
-  if (!["1", "2"].includes(this.tipo)) {
+  if (!["income", "expense"].includes(this.tipo)) {
     return {
       ok: false,
       message: "El valor tipo es erroneo",
@@ -40,63 +90,62 @@ Movimiento.prototype.validarMovimiento = function () {
   };
 };
 
-const transacciones = [];
+// function registrarIngresoOEgreso() {
+//   while (true) {
+//     const descripcion = prompt("Ingrese la nueva transacción");
+//     const tipoDeTransaccion = prompt(
+//       "Escoja el tipo de transacción \n1) Ingreso\n2) Egreso\n\n Solo debe poner el número de la opción"
+//     );
+//     const monto = prompt("Ingrese el monto de la transacción");
+//     // antes de insertar el moviento al arreglo debo crear el objeto y ejecutar la validacion
+//     const moviento = new Movimiento(
+//       tipoDeTransaccion,
+//       Number(monto),
+//       descripcion
+//     );
 
-function registrarIngresoOEgreso() {
-  while (true) {
-    const descripcion = prompt("Ingrese la nueva transacción");
-    const tipoDeTransaccion = prompt(
-      "Escoja el tipo de transacción \n1) Ingreso\n2) Egreso\n\n Solo debe poner el número de la opción"
-    );
-    const monto = prompt("Ingrese el monto de la transacción");
-    // antes de insertar el moviento al arreglo debo crear el objeto y ejecutar la validacion
-    const moviento = new Movimiento(
-      tipoDeTransaccion,
-      Number(monto),
-      descripcion
-    );
+//     const validacion = moviento.validarMovimiento();
 
-    const validacion = moviento.validarMovimiento();
+//     if (!validacion.ok) {
+//       alert(validacion.message);
+//     } else {
+//       // si es true entonces agregamo el moviento a transacciones
+//       transacciones.push(moviento);
+//       // llamar a movieminto.render()
+//     }
 
-    if (!validacion.ok) {
-      alert(validacion.message);
-    } else {
-      // si es true entonces agregamo el moviento a transacciones
-      transacciones.push(moviento);
-    }
+//     // transacciones.push({
+//     //   transaccion,
+//     //   tipoDeTransaccion,
+//     //   monto,
+//     //   fechaDeCreacion: new Date(),
+//     // });
 
-    // transacciones.push({
-    //   transaccion,
-    //   tipoDeTransaccion,
-    //   monto,
-    //   fechaDeCreacion: new Date(),
-    // });
+//     const confirmacion = confirm("Desea agregar otra transacción?");
+//     // ok => true: continuar con otra transaccion
+//     // cancel => false: terminar la transaccion
+//     // en que caso deberiamos detener el while
+//     if (confirmacion === false) {
+//       // detener el while
+//       break;
+//     }
+//   }
+// }
 
-    const confirmacion = confirm("Desea agregar otra transacción?");
-    // ok => true: continuar con otra transaccion
-    // cancel => false: terminar la transaccion
-    // en que caso deberiamos detener el while
-    if (confirmacion === false) {
-      // detener el while
-      break;
-    }
-  }
-}
+// function mapTransactionNames() {
+//   const names = transacciones.map(function (transaccion) {
+//     return transaccion.transaccion;
+//   });
+//   console.log(names);
+// }
 
-function mapTransactionNames() {
-  const names = transacciones.map(function (transaccion) {
-    return transaccion.transaccion;
-  });
-  console.log(names);
-}
+// function filterTransactions() {
+//   // condiciones egreso y > 100
+//   const filtroDeDatos = transacciones.filter(
+//     (transaccion) =>
+//       transaccion.monto > 100 && transaccion.tipoDeTransaccion === "2"
+//   );
+//   console.log(filtroDeDatos);
+// }
 
-function filterTransactions() {
-  // condiciones egreso y > 100
-  const filtroDeDatos = transacciones.filter(
-    (transaccion) =>
-      transaccion.monto > 100 && transaccion.tipoDeTransaccion === "2"
-  );
-  console.log(filtroDeDatos);
-}
-
-registrarIngresoOEgreso();
+// registrarIngresoOEgreso();
